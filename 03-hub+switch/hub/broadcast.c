bfc831ed6867232b1d9f714a9c86ce4e06e6c20c
Haochen Qiu <josephqiu@berkeley.edu>
2022-09-12 16:26:52 +0800

    start lab 3

---
+++ 03-hub+switch/hub/broadcast.c
@@ -0,0 +1,11 @@
+#include "base.h"
+#include <stdio.h>
+
+extern ustack_t *instance;
+
+// the memory of ``packet'' will be free'd in handle_packet().
+void broadcast_packet(iface_info_t *iface, const char *packet, int len)
+{
+	// TODO: broadcast packet 
+	fprintf(stdout, "TODO: broadcast packet.\n");
+}
