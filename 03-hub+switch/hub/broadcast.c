54b99ac24b006dd58ecdc03021ff56a26b8d435e
Haochen Qiu <josephqiu@berkeley.edu>
2022-09-13 15:55:47 +0800

    sync to server

--- 03-hub+switch/hub/broadcast.c
+++ 03-hub+switch/hub/broadcast.c
@@ -6,9 +6,8 @@ extern ustack_t *instance;
 // the memory of ``packet'' will be free'd in handle_packet().
 void broadcast_packet(iface_info_t *iface, const char *packet, int len)
 {
-	// TODO: broadcast packet 
 	iface_info_t *node;
-	list_for_each_entry(node, instance -> list, list) {
+	list_for_each_entry(node, &iface -> list, list) {
 		if (node != iface) {
 			iface_send_packet(node, packet, len);
 		}
